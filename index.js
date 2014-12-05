75dfa4d249c747e7423130a0e36a2f431bc262b0
Hayagriv <hags37@gmail.com>
2014-12-05 01:11:07 -0800

    trial learning module skeleton built

---
+++ index.js
@@ -0,0 +1,37 @@
+// set up =================================================
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+module.exports = {
+// set the static files location (ex: /public/img will be /img for users)
+app.use(express.static(__dirname + '/client'));
+}
+// return route config function
+module.exports = function (app, passport, express) {
+
+	var router = express.Router();
+
+	router.use(
+		'/learningMethod' 
+		function (req, res) {
+			res.sendFile(
+				path.resolve(
+					__dirname,
+					'client/trial.html'
+					
+				)
+			);
+		}
+	);
+
+
+	return router;
+
+}
